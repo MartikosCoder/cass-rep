8beabfe5b82dcc9349540d3341d5694154b884a5
anz81 <59411601+anz81@users.noreply.github.com>
2022-04-13 11:40:50 +0300

    Delete iiko directory

--- iiko/consts.js
+++
@@ -1,9 +0,0 @@
-const iikoServer = "http://95.31.10.45:9080/resto/api";
-const iikoLogin = "1";
-const iikoPassword = "356a192b7913b04c54574d18c28d46e6395428ab";
-
-module.exports = {
-  iikoServer,
-  iikoLogin,
-  iikoPassword
-}
