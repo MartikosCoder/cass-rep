63c8e3a7a2be629f777b2651e15ae7ec53171bd2
Alexander Zubarev <anzubarev81@yandex.ru>
2022-06-09 17:08:12 +0300

    fix count of bills

--- src/iiko/getFilters.js
+++ src/iiko/getFilters.js
@@ -7,21 +7,21 @@ const getFilters = async (token, iikoServer) => {
     discountTypes: [],
     orderTypes: []
   }
-  let result = await axios.get(`${iikoServer}/v2/entities/list?rootType=PaymentType&key=${token}`);
+  let result = await axios.get(`${iikoServer}/v2/entities/list?rootType=PaymentType&includeDeleted=false&key=${token}`);
   if (result.status != 200) {
     console.log('Error getting PaymentTypes from iiko:');
     console.log(result.statusText);
     return null;
   }
   result.data.forEach(el => filters.payTypes.push(el.name));
-  result = await axios.get(`${iikoServer}/v2/entities/list?rootType=DiscountType&key=${token}`);
+  result = await axios.get(`${iikoServer}/v2/entities/list?rootType=DiscountType&includeDeleted=false&key=${token}`);
   if (result.status != 200) {
     console.log('Error getting DiscountTypes from iiko:');
     console.log(result.statusText);
     return null;
   }
   result.data.forEach(el => filters.discountTypes.push(el.name));
-  result = await axios.get(`${iikoServer}/v2/entities/list?rootType=OrderType&key=${token}`);
+  result = await axios.get(`${iikoServer}/v2/entities/list?rootType=OrderType&includeDeleted=false&key=${token}`);
   if (result.status != 200) {
     console.log('Error getting orderTypes from iiko:');
     console.log(result.statusText);
